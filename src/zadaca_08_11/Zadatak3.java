269b5ee724f6cfe3f1e7db132b9823c45c684269
hadzi <hadzi@DESKTOP-503J8PP>
2017-11-08 23:48:00 +0100

    Napisati program koji ispisuje sve brojeve u rangu od 1 do 1000 koji sudjeljivi sa 4, ali ne sa 5.

---
+++ src/zadaca_08_11/Zadatak3.java
@@ -0,0 +1,14 @@
+package zadaca_08_11;
+
+public class Zadatak3 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=1000;++i) {
+			if(i%4==0 && i%5!=0)
+				System.out.print(i+" ");
+		}
+
+	}
+
+}
